b3262f438c6fe0266dc69a5164961be44341f9fb
rkypragada <rkypragada@gmail.com>
2026-07-09 15:36:55 +0530

    Initial Commit

---
+++ src/main/java/taskmaster/Main.java
@@ -0,0 +1,12 @@
+package taskmaster;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to TaskMaster");
+
+    }
+
+}
+
---
+++ src/main/java/taskmaster/Main1.java
@@ -0,0 +1,11 @@
+package taskmaster;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+
+        DBConnection.getConnection();
+
+    }
+
+}
